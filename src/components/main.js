8e1d0edaf0075c35d5e4ca2b2e0d25ecc6d26c47
Ramya-V <rajaramya559@gmail.com>
2021-06-22 13:05:27 +0530

    initial commit

---
+++ src/components/main.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+const Main = (props) => {
+
+    return(
+        <Container>
+            <ShareBox>Share</ShareBox>
+        </Container>
+    )
+    
+};
+
+// ---------------------------------------- //
+//              Styled Components           //
+// ---------------------------------------- //
+/* Container */
+const Container = styled.div`
+    grid-area: mainPart;
+`;
+
+/* Common Card */
+const CommonCard = styled.div`
+    text-align: center;
+    overflow: hidden;
+    margin-bottom: 8px;
+    background-color: #fff;
+    border-radius: 5px;
+    position: relative;
+    border: none;
+    box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rbg(0 0 0 / 20%);
+`;
+
+/* Share Box */
+const ShareBox = styled(CommonCard)`
+    display: flex;
+    flex-direction: column;
+    color: #958b7b;
+    margin: 0 0 8px;
+    background: white;
+`;
+
+export default Main;
